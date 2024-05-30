fb1048996dcb74a8fb074ad1ff5ac71463c6d436
Kim Dong Yeon <109346159+KDDYYY@users.noreply.github.com>
2024-05-30 22:37:41 +0900

    24-05-30
    fix : FcmDTO 오류 수정

--- src/main/java/com/mementee/api/dto/notificationDTO/FcmDTO.java
+++ src/main/java/com/mementee/api/dto/notificationDTO/FcmDTO.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class FcmDTO {
-    private final Long notificationId;
+    private Long targetMemberId;
 
     private Long senderId;
     private String senderName;
