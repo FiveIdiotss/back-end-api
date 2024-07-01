50b77c0ef63faa9c88318d07dd22f07326df8d78
Kim Dong Yeon <do121712@naver.com>
2024-07-01 17:53:14 +0900

    feat : Apply 거절에 대한 필드 추가와 거절 사유에 입력에 대한 메소드 추가

--- src/main/java/com/team/mementee/api/domain/enumtype/ApplyState.java
+++ src/main/java/com/team/mementee/api/domain/enumtype/ApplyState.java
@@ -1,5 +1,22 @@
 package com.team.mementee.api.domain.enumtype;
 
+import lombok.Getter;
+
+@Getter
 public enum ApplyState {
-    HOLDING, COMPLETE
+
+    HOLDING("대기중"),
+    COMPLETE("신청 수락"),
+    REJECT("신청 거절");
+
+    String content;
+
+    ApplyState(String content) {
+        this.content = content;
+    }
+
+    public void reasonOfReject(String content){
+        this.content = content;
+    }
+
 }
