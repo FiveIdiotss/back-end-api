2321dec9724acfb2daf8570f787315e8f5fa4e28
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-04 00:11:27 -0400

    fixed: pay method deleted.

--- src/main/java/com/mementee/api/controller/PaymentController.java
+++ src/main/java/com/mementee/api/controller/PaymentController.java
@@ -1,7 +1,6 @@
 package com.mementee.api.controller;
 
 import com.siot.IamportRestClient.IamportClient;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,11 +9,6 @@ public class PaymentController {
     public PaymentController() {
         IamportClient iamportClient = new IamportClient("0328847288412452",
                 "lhpSmGFRJeLvX2ycger4sel2I48qFyYWaZiEcvyBzAYJIx2XqBHvrx1wOtBWIE8JQiViWtvaiZ2Y585U");
-    }
-
-    @PostMapping("/verifyIamport/{rsp.imp_uid}")
-    public void pay() {
-
     }
 
 
