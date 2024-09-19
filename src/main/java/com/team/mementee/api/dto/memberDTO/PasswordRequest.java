f44cb2ce1c239b10637ee9327b94ffb1852f3a55
Kim Dong Yeon <do121712@naver.com>
2024-09-19 20:32:27 +0900

    refactor : PasswordRequest 생성자 제거

--- src/main/java/com/team/mementee/api/dto/memberDTO/PasswordRequest.java
+++ src/main/java/com/team/mementee/api/dto/memberDTO/PasswordRequest.java
@@ -1,10 +1,8 @@
 package com.team.mementee.api.dto.memberDTO;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class PasswordRequest {
     private String password;
 }
