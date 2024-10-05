53e5c2a10e1735e7d950abf2e6c1f43c4cae704f
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-10-05 04:19:41 -0400

    Refactor FileService methods and imports
    Remove unnecessary S3-related imports and `createObjectMetaData` method. Refactor `getExtension` and `getFileType` methods to work with MultipartFile type and switch to enhanced switch-case for more clarity in `extractFileType` method.

--- src/main/java/com/team/mementee/api/service/FileService.java
+++ src/main/java/com/team/mementee/api/service/FileService.java
@@ -1,6 +1,5 @@
 package com.team.mementee.api.service;
 
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.team.mementee.api.domain.enumtype.MessageType;
 import ezvcard.Ezvcard;
 import ezvcard.VCard;
@@ -8,12 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.UUID;
 
 import static com.team.mementee.api.domain.enumtype.MessageType.*;
@@ -24,42 +23,30 @@ import static com.team.mementee.api.domain.enumtype.MessageType.*;
 public class FileService {
 
     @NotNull
-    public static ObjectMetadata createObjectMetaData(Long contentLength, String contentType) {
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(contentLength);
-        metadata.setContentType(contentType);
-        return metadata;
-    }
-
-    @NotNull
     public String generateFileName(String extension) {
         return UUID.randomUUID() + "." + extension;
     }
 
     @NotNull
-    public String getExtension(String fileName) {
+    public String getExtension(MultipartFile file) {
+        String fileName = Objects.requireNonNull(file.getOriginalFilename());
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
-    public MessageType getFileType(String contentType) {
-        if (contentType.startsWith("image")) return IMAGE;
-        if (contentType.startsWith("video")) return VIDEO;
-        if ("application/pdf".equals(contentType)) return PDF;
-        if ("application/zip".equals(contentType)) return ZIP;
-        if ("text/vcard".equals(contentType)) return CONTACT;
-        else return FILE;
-    }
+    public MessageType extractFileType(MultipartFile file) {
+        String contentType = file.getContentType();
+        if (contentType == null) return FILE;
 
-    public String extractFileNameFromUrl(String fileUrl) {
-        try {
-            URL url = new URL(fileUrl);
-            String path = url.getPath();
-            return path.substring(path.lastIndexOf('/') + 1);
-        } catch (MalformedURLException e) {
-            log.error("Error extracting file name from URL: {}", e.getMessage(), e);
-            return null;
-
-        }
+        return switch (contentType) {
+            case "application/pdf" -> PDF;
+            case "application/zip" -> ZIP;
+            case "text/vcard" -> CONTACT;
+            default -> {
+                if (contentType.startsWith("image")) yield IMAGE;
+                if (contentType.startsWith("video")) yield VIDEO;
+                yield FILE;
+            }
+        };
     }
 
     public VCard readVCard(String filePath) throws IOException {
