111c4c7c8867a1f28974c6d7b190f2df42b8b210
Kim Dong Yeon <do121712@naver.com>
2024-11-18 23:50:18 +0900

    chore : Swagger Server 설정 수정

--- src/main/java/com/team/mementee/config/SwaggerConfig.java
+++ src/main/java/com/team/mementee/config/SwaggerConfig.java
@@ -4,10 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SecurityScheme(
         name = "Bearer Authentication",
@@ -24,10 +24,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("멘토 멘티 API")
-                        .description("멘토 멘티 매칭")
-                        .version("1.0.0"));
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("멘토 멘티 API")
+                .description("멘토 멘티 매칭")
+                .version("1.0.0");
     }
 }
 
