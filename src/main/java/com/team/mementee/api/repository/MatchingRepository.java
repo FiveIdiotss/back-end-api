ab1f5132b191a807fb9b75d2beea2be9371f308d
Kim Dong Yeon <do121712@naver.com>
2024-12-04 23:52:04 +0900

    refactor : 상담에 대한 정보 조회시 작성 멘토/멘티 정보 동시 조회 Fetch Join 전략을 사용해 불필요한 쿼리문 개선

--- src/main/java/com/team/mementee/api/repository/MatchingRepository.java
+++ src/main/java/com/team/mementee/api/repository/MatchingRepository.java
@@ -3,14 +3,22 @@ package com.team.mementee.api.repository;
 import com.team.mementee.api.domain.Matching;
 import com.team.mementee.api.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
 
-    List<Matching> findMatchingsByMentor(Member mentor);
+    @Query("SELECT ma FROM Matching ma " +
+            "JOIN FETCH ma.mentor m " +
+            "WHERE m = :mentor")
+    List<Matching> findMatchingsByMentor(@Param("mentor") Member mentor);
 
-    List<Matching> findMatchingsByMentee(Member mentee);
+    @Query("SELECT ma FROM Matching ma " +
+            "JOIN FETCH ma.mentee m " +
+            "WHERE m = :mentee")
+    List<Matching> findMatchingsByMentee(@Param("mentee") Member mentee);
 
 
 }
