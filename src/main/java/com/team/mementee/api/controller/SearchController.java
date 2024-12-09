e355d3f19e5710aeb1295a098599e9a1ec224cd5
Kim Dong Yeon <do121712@naver.com>
2024-12-09 20:09:45 +0900

    refactor : HTML 태그를 제외 후 항목만 검색되도록 추가

--- src/main/java/com/team/mementee/api/controller/SearchController.java
+++ src/main/java/com/team/mementee/api/controller/SearchController.java
@@ -9,6 +9,7 @@ import com.team.mementee.api.service.SubBoardService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,13 +25,18 @@ public class SearchController {
     private final BoardService boardService;
     private final SubBoardService subBoardService;
 
+    public String sanitizeHtml(String input) {
+        return Jsoup.clean(input, "", org.jsoup.safety.Safelist.none(), new org.jsoup.nodes.Document.OutputSettings().prettyPrint(false));
+    }
+
     // 제목, 내용, 학과
     @GetMapping("/api/search")
     public CommonApiResponse<?> getSearchFilters(@RequestParam String query) {
-        List<Board> boards_title = boardService.findAllByTitleContaining(query);
-        List<Board> boards_content = boardService.findAllByContentContaining(query);
-        List<SubBoard> subBoards_title = subBoardService.findAllByTitleContaining(query);
-        List<SubBoard> subBoards_content = subBoardService.findAllByContentContaining(query);
+        String sanitizedQuery = sanitizeHtml(query);
+        List<Board> boards_title = boardService.findAllByTitleContaining(sanitizedQuery);
+        List<Board> boards_content = boardService.findAllByContentContaining(sanitizedQuery);
+        List<SubBoard> subBoards_title = subBoardService.findAllByTitleContaining(sanitizedQuery);
+        List<SubBoard> subBoards_content = subBoardService.findAllByContentContaining(sanitizedQuery);
         return CommonApiResponse.createSuccess(SearchDTO.toEntity(boards_title, subBoards_title, boards_content, subBoards_content));
     }
 
